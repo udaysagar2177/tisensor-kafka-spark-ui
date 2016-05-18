9d9d4f564b5569c3731ee1d053188216553715ad
udaysagar2177 <udaysagar.2177@gmail.com>
2016-05-18 09:59:40 -0700

    Add python launch script with maestro functions for rest-api

--- rest-api/run.py
+++ rest-api/run.py
@@ -18,11 +18,11 @@ if LOAD_GENERATION and bool(LOAD_GENERATION) is True:
     REST_URL = 'http://' + REST_URL_LIST[random.randrange(0, len(REST_URL_LIST))] + '/datapoint'
     SIMULATED_TISENSOR_COUNT = os.getenv('SIMULATED_TISENSOR_COUNT', 5)
     SIMULATED_TISENSOR_ID_HANDLE = os.getenv('SIMULATED_TISENSOR_ID_HANDLE', 'TISENSOR_DEFAULT_ID_')
-    os.execl('/usr/bin/mvn', '-e', '-X', 'compile', 'exec:java', '-Dexec.mainClass=com.load.SimulatedTiSensor', '-Dexec.args='+REST_URL+' '+SIMULATED_TISENSOR_COUNT+' '+SIMULATED_TISENSOR_ID_HANDLE)
+    os.execl('/usr/bin/mvn', '-e', '-X', 'exec:java', '-Dexec.mainClass=com.load.SimulatedTiSensor', '-Dexec.args='+REST_URL+' '+SIMULATED_TISENSOR_COUNT+' '+SIMULATED_TISENSOR_ID_HANDLE)
 else:
     # Start the REST API.
     KAFKA_BROKER_LIST = ','.join(get_node_list('kafka', ports=['broker']))
     os.environ['KAFKA_BROKER_LIST'] = KAFKA_BROKER_LIST
-    os.execl('/usr/bin/mvn', 'clean', 'install',  'tomcat7:run',
+    os.execl('/usr/bin/mvn', '-X', 'tomcat7:run',
              '-Dmaven.tomcat.port=' +
              (str)(get_port('rest')))
